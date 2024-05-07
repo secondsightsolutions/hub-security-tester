45bac750b76c4628342c0be7bcfebc5471913e9f
Matthew Lee <mlee@MMLEE1.local>
2024-05-07 10:54:04 -0700

    Initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Security Validation
+This repo hosts a variety of vulnerable codebases, as well as code smells. This will be use for testing various services
---
+++ code_smells/vulnerable_python.py
@@ -0,0 +1,10 @@
+# Code smell #1
+variable_a = 1
+variable_b = 3
+if variable_a == 1:
+   if variable_b == 3:
+      print("Code smell example")
+
+# Code smell #2
+def function_a():
+   pass
